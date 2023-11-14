25a7a494213cbaadb9a10c5ff67d2af692cc2f1f
Chef03 <johnmurphyz166@gmail.com>
2023-11-14 20:38:17 +0100

    #1 Demonstrate different approaches for service communication

--- main.go
+++ main.go
@@ -1,25 +1,15 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
-
-type User struct {
-	ID       primitive.ObjectID `bson:"_id,omitempty"`
-	Username string             `bson:"username,omitempty"`
-	Password string             `bson:"password,omitempty"`
-}
 
 var client *mongo.Client
 
@@ -29,67 +19,44 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
+	// Connect to MongoDB
 	connectToDB()
-	http.HandleFunc("/api/", handler)
-	http.HandleFunc("/users", getUsers)
-	fmt.Println("Server is listening for connections...")
+
+	// Variant #1 MQTT
+	/*
+		In the likely scenario that REST will only be adhered to between the client and the main gateway; The communication between this module will
+		most likely happen through a more efficient protocol like MQTT or HTTP 2.0. In that case, an MQTT client can be found below
+		which can subscribe and publish to different topics and handle events
+	*/
+	mqttClient := getInstance()
+	mqttClient.Subscribe("/login", byte(0), func(c mqtt.Client, m mqtt.Message) {
+		// Potentially parse a JSON payload in the form { username: string, password: string }
+		fmt.Printf("Recieved message %s\n", string(m.Payload()[:]))
+	})
+
+	// Variant #2 REST
+	/*
+		This endpoint listens for a request which contains a username and password field in its headers
+		it then queries the database and proceeds to hash the given password to the one present in the database
+		finally it responds with the status of the operation
+	*/
+	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		username := r.Header.Get("username")
+		password := r.Header.Get("password")
+		status := login(username, password)
+		fmt.Fprint(w, status)
+	})
+
+	http.HandleFunc("/signup", func(w http.ResponseWriter, r *http.Request) {
+
+		username := r.Header.Get("username")
+		password := r.Header.Get("password")
+		status := signup(username, password)
+		fmt.Fprint(w, status)
+
+	})
+
+	fmt.Println("HTTP server is listening for requests...")
 	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
-}
-
-func getUsers(w http.ResponseWriter, r *http.Request) {
-
-	var users []User
-	col := client.Database("dentanoid").Collection("users")
-	doc, err := col.Find(context.TODO(), bson.D{{}})
-	if err != nil {
-		panic(err)
-	}
-
-	err = doc.All(context.TODO(), &users)
-	if err != nil {
-		panic(err)
-	}
-
-	result, err := json.Marshal(users)
-	fmt.Printf("Sending users.\n")
-	fmt.Fprint(w, string(result))
-
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	username := r.URL.Path[1:]
-	addUser(username, "password")
-}
-
-func connectToDB() {
-
-	c, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(os.Getenv("MONGO_URI")))
-	client = c
-
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Connected to MongoDB")
-
-}
-
-func addUser(username string, password string) {
-
-	col := client.Database("dentanoid").Collection("users")
-
-	if col == nil {
-		fmt.Println("Collection does not exist")
-	}
-
-	doc := User{Username: username, Password: password}
-
-	result, err := col.InsertOne(context.TODO(), doc)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Printf("Result ID: %v \n", result.InsertedID)
-	fmt.Printf("Added User with username %s and password %s\n", username, password)
 
 }
