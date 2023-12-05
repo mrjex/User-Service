25d48398e728766160c39ebff58edd00444a6623
Lucas Holter <holter@chalmers.se>
2023-12-05 21:12:51 +0100

    #4 (Ongoing) Add lines that tells patient struct how to behave when being converted into json and bson

--- schemas/Patient.go
+++ schemas/Patient.go
@@ -4,7 +4,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Patient struct {
     ID       primitive.ObjectID `bson:"_id,omitempty"`
-    Username string
-    Password string
+    Username string  `bson:"username,omitempty"`
+    Password string `bson:"password,omitempty" json:"password,omitempty"`
 }
 
