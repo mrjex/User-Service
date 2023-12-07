c3eeaaaef2a72885cd8add2b02d2049b1e12ea1e
Lucas Holter <holter@chalmers.se>
2023-12-07 10:35:41 +0100

    #4 (Ongoing) Change dentist tests do adhere to new method signatures

--- tests/dentist_test.go
+++ tests/dentist_test.go
@@ -2,15 +2,26 @@ package tests
 
 import (
 	"Group20/Dentanoid/controllers"
+	"Group20/Dentanoid/schemas"
 	"testing"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func TestCreate(t *testing.T) {
-	result := controllers.CreateDentist("mike", "password", client)
+    dentist := schemas.Dentist{
+        Username: "mike",
+        Password: "password",
+        ID: primitive.NewObjectID(),
+    }
+
+    var returnData controllers.Res
+
+	result := controllers.CreateDentist(dentist, returnData, client)
 	if !result {
 		t.Error("Dentist Creation Failed")
 	}
-	result = controllers.DeleteDentist("mike", client)
+	result = controllers.DeleteDentist(dentist.Username, returnData, client)
 	if !result {
 		t.Error("Dentist Deletion Failed")
 	}
