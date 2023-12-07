5406cf2e77701a7946c9fac79b1ef050ff5b59dd
Lucas Holter <holter@chalmers.se>
2023-12-07 15:28:27 +0100

    #4 (Ongoing) Add json encoding for patient and dentist

--- schemas/Patient.go
+++ schemas/Patient.go
@@ -3,7 +3,7 @@ package schemas
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Patient struct {
-    ID       primitive.ObjectID `bson:"_id,omitempty"`
+    ID       primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
     Username string  `bson:"username,omitempty" json:"username,omitempty"`
     Password string `bson:"password,omitempty" json:"password,omitempty"`
 }
