00e33eb019f48d019ac5e7490a055ebb10f962e8
Lucas Holter <holter@chalmers.se>
2023-12-07 12:07:42 +0100

    #4 (Ongoing) Add json convertion field names for dentist and patient

--- schemas/Dentist.go
+++ schemas/Dentist.go
@@ -4,6 +4,6 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Dentist struct {
 	ID       primitive.ObjectID `bson:"_id,omitempty"`
-	Username string             `bson:"username,omitempty"`
-	Password string             `bson:"password,omitempty"`
+	Username string             `bson:"username,omitempty" json:"username,omitempty"`
+	Password string             `bson:"password,omitempty" json:"password,omitempty"`
 }
--- schemas/Patient.go
+++ schemas/Patient.go
@@ -4,7 +4,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Patient struct {
     ID       primitive.ObjectID `bson:"_id,omitempty"`
-    Username string  `bson:"username,omitempty"`
+    Username string  `bson:"username,omitempty" json:"username,omitempty"`
     Password string `bson:"password,omitempty" json:"password,omitempty"`
 }
 
